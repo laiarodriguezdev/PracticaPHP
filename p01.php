dc49b840c41a1e22b0fbf1f84c8d29931c1b834b
LaiaRodri <laia.rodriguez@enti.cat>
2023-09-21 17:26:50 +0200

    Fixing array

--- p01.php
+++ p01.php
@@ -6,6 +6,38 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+
+        $nom = 'Kurt';
+        $cognom = 'Cagle';
+        $edat = 40;
+        $dataNeix = new DateTime('1983-05-12');
+        $tel = array('654332211', '9333332211');
+        $adPostal = 'Carrer de Turin, 15';
+        $adElec = 'kurt.cagle@example.com';
+        $treballa = true;
+        $alt = 1.67;
+    ?>
     
+    <h1>describint 
+        <?php echo $nom, ' ', $cognom  ?>
+    </h1>
+    <h2>Les dades de  
+        <?php echo $nom?>
+        son:
+    </h2>
+
+    <ul>
+        <li>Es diu: <?php echo $nom  ?></li>
+        <li>Te <?php echo $edat  ?> anys</li>
+        <li>Va neixer l'any: <b><?php echo $dataNeix  ?></b></li>
+        <li>Els seus telèfons són: <?php echo $tel  ?></li>
+        <li>Viu a: <?php echo $nom  ?></li>
+        <li>El seu email és: <?php echo $adElec  ?></li>
+        <li>Treballa: <?php echo $treballa  ?></li>
+        <li>Medeix: <?php echo $alt  ?></li>
+
+    </ul>
+
 </body>
 </html>
