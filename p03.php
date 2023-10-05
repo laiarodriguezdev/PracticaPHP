62ed78cd8ce6db57b93b8a3707f76ec5b15998d5
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-05 18:42:49 +0200

    Informacio and P03 new files

---
+++ p03.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="informacio.php" method="post">
+        <label for="nom">Posa el nom del restaurant</label>
+        <input type="text" value="nom" id="nom" name="" placeholder="Posa el teu nom">
+
+        <br>
+        
+        <label for="nom">Posa el nom del restaurant</label>
+        <input type="number" value="nom" id="nom" name="" placeholder="Posa el número de clients">
+    
+        <br>
+
+        <input type="submit" value="enviar">
+
+    </form>
+</body>
+</html>
