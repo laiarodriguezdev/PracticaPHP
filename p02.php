0ead2c27b533189423c2da69420adf4a75bf7058
LaiaRodri <laia.rodriguez@enti.cat>
2023-10-05 17:04:59 +0200

    Solucions donades

--- p02.php
+++ p02.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
     <body>
         <!---------------------------Part individual -------------------------->
@@ -83,8 +83,5 @@
             }
             ?>
         </div>
-
-
     </body>
-
 </html>
